06c20f55a36a8f43332d9a26394d969b33d9165a
emailboxsachin <emailbox.sachin@rediffmail.com>
2020-11-18 19:22:05 +0530

    removed commented code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
-// const db = require('./queries.js')
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -19,11 +18,6 @@ for (const ent of entity){
 	app.use(routes);
 }
 
-// app.get('/users', db.getUsers)
-// app.get('/users/:id', db.getUserById)
-// app.post('/users', db.createUser)
-// app.put('/users/:id', db.updateUser)
-// app.delete('/users/:id', db.deleteUser)
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 

c08ca306eae741991be8a2c75088f62ad840a714
emailboxsachin <emailbox.sachin@rediffmail.com>
2020-11-18 17:37:47 +0530

    db new code

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const bodyParser = require('body-parser')
-// const pool = require('./db.js')
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const pool = require('./db.js')
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -18,20 +18,29 @@ app.use(bodyParser.urlencoded({extended:true,}))
 app.get('/', (request, response) => {
   response.sendFile( __dirname +"/views/index.html" )
 })
+// app.get('/users', async (request,response)=>{
+// 	// response.status(200).json([{"id":77,"name":"appu","email":"appu@gmail.com"},{"id":76,"name":"anna","email":"anna@gmail.com"}])
+// 	try {
+//       const client = await pool.connect();
+//       const result = await client.query('SELECT * FROM users ORDER BY id DESC');
+//       console.log(JSON.stringify(result))
+//       // const results = { 'results': (result) ? result.rows : null};
+//       // res.render('pages/db', results );
+//       response.status(200).json(result.rows)
+//       client.release();
+//     } catch (err) {
+//       console.error(err);
+//       res.send("Error " + err);
+//     }
+// })
+
 app.get('/users', async (request,response)=>{
-	// response.status(200).json([{"id":77,"name":"appu","email":"appu@gmail.com"},{"id":76,"name":"anna","email":"anna@gmail.com"}])
-	try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM users ORDER BY id DESC');
-      console.log(JSON.stringify(result))
-      // const results = { 'results': (result) ? result.rows : null};
-      // res.render('pages/db', results );
-      response.status(200).json(result.rows)
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
+	pool.query('SELECT * FROM users ORDER BY id DESC', (error, results) => {
+	    if (error) {
+	      throw error
+	    }
+	    response.status(200).json(results.rows)
+ 	})
 })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

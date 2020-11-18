83550bda9e250d202919bf3a333e5f7d65bb6edc
emailboxsachin <emailbox.sachin@rediffmail.com>
2020-11-18 19:18:37 +0530

    arranged db access in folder

--- api/users/routes.js
+++ api/users/routes.js
@@ -6,7 +6,7 @@ router.get('/users', model.getUsers)
 router.get('/users/:id', model.getUserById)
 router.post('/users', model.createUser)
 router.delete('/users/:id', model.deleteUser)
-router.put('/users/:id', db.updateUser)
+router.put('/users/:id', model.updateUser)
 
 
 module.exports = router;

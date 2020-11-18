174d8562b901d1f4ea3abed6e883d5ec8971f28d
emailboxsachin <emailbox.sachin@rediffmail.com>
2020-11-18 15:22:33 +0530

    index.js changed

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true,}))
+app.get('/', (request, response) => {
+  response.sendFile( __dirname +"/views/index.html" )
+})
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))

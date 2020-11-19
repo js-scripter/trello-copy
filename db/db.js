fd1010b6f0b70881fb116f7c86d8ff78ede09707
emailboxsachin <emailbox.sachin@rediffmail.com>
2020-11-19 14:47:56 +0530

    changed db access so that local and online db will work

--- db/db.js
+++ db/db.js
@@ -1,8 +1,25 @@
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+// module.exports = pool;
+
+require('dotenv').config()
+
+const {Pool} = require('pg')
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+console.log(connectionString)
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: {
     rejectUnauthorized: false
   }
-});
-module.exports = pool;
+})
+
+module.exports = pool
